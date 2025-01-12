1ba2efefcfd79cce35986645d3cfb6786cbf9875
JesserHamdaoui <107654958+JesserHamdaoui@users.noreply.github.com>
2025-01-12 22:14:55 +0100

    refactor: Refactor the main sketch

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import "@/styles/global.css";
 import {
   Engine,
   Runner,
@@ -7,21 +6,28 @@ import {
   MouseConstraint,
   Mouse,
 } from "matter-js";
-
 import p5 from "p5";
+import { Ball } from "./components/Ball";
 
 new p5((p) => {
-  let engine;
-  let runner;
+  let engine: Engine;
+  let runner: Runner;
+  let ball: Ball;
 
   p.setup = () => {
     p.createCanvas(1200, 800);
+
     engine = Engine.create();
     runner = Runner.create();
     Runner.run(runner, engine);
+
+    ball = new Ball(p, engine, 600, 400);
   };
 
   p.draw = () => {
     p.background(167, 213, 229);
+    Engine.update(engine);
+
+    ball.display();
   };
 });

f205d4464d4b28dd7f82c05118fc55b0df73903e
JesserHamdaoui <107654958+JesserHamdaoui@users.noreply.github.com>
2025-01-13 21:52:55 +0100

    feat: Add QuantativeSlider component

---
+++ src/components/Sliders/QuantativeSlider.ts
@@ -0,0 +1,15 @@
+export const QuantativeSlider = (
+  p: p5,
+  min: number,
+  max: number,
+  defaultValue: number,
+  x: number,
+  y: number,
+  onChange: Function
+) => {
+  const slider = p.createSlider(min, max, defaultValue);
+  slider.position(x, y);
+  slider.input(() => onChange(slider.value()));
+
+  return slider;
+};

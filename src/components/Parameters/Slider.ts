6644bc45c5957d60bca5f1c4dd42b04c6fb3f830
JesserHamdaoui <107654958+JesserHamdaoui@users.noreply.github.com>
2025-02-03 12:06:56 +0100

    fix: Fix fixed decimals of frequency to 2 and Add indicators to the sliders

--- src/components/Parameters/Slider.ts
+++ src/components/Parameters/Slider.ts
@@ -5,6 +5,8 @@ interface SliderProps {
   label: string;
   min: number;
   max: number;
+  minIndicator: string;
+  maxIndicator: string;
   step: number;
   initialValue: number;
   onChange: (value: number) => void;
@@ -23,6 +25,9 @@ export class Slider {
     const label = p.createSpan(props.label);
     label.addClass("slider-label");
 
+    const lowLabel = p.createSpan(props.minIndicator);
+    lowLabel.addClass("indicator");
+
     const slider = p.createSlider(
       props.min,
       props.max,
@@ -32,8 +37,13 @@ export class Slider {
     slider.addClass("slider-input");
     (slider as any).input(() => props.onChange(slider.value() as number));
 
+    const highLabel = p.createSpan(props.maxIndicator);
+    highLabel.addClass("indicator");
+
     this.container.child(label);
+    this.container.child(lowLabel);
     this.container.child(slider);
+    this.container.child(highLabel);
 
     if (this.sliderContainer) {
       this.sliderContainer.child(this.container);
